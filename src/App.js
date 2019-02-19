6fd4c9ea5eed8c25a383e975cf772e1842208565
Rahul Makhija <rahuls360@gmail.com>
2019-02-19 22:39:48 +0530

    Store input box contents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    wish: ""
+  }
+
+  inputChanged = (event)=> {
+    let wish = this.state.wish;
+    wish = this.wishRef.current.value;
+    this.setState({wish: wish});
+  } 
+
+  wishRef = React.createRef();
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+          <h3>What is your wish</h3>
+          <input type="text" name="wish" ref={this.wishRef} onChange={this.inputChanged}/>
       </div>
     );
   }
