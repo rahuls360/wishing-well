0fe9e8a42a1c07ecc98a14bb2c98c3a5bd5ae15d
Rahul Makhija <rahuls360@gmail.com>
2019-02-20 11:51:35 +0530

    Check if hard working or not

--- src/components/Worthy.js
+++ src/components/Worthy.js
@@ -4,16 +4,25 @@ class Worthy extends React.Component{
     deserveItRef = React.createRef();
     specficActionsRef = React.createRef();
 
+
+    handleWorthy = (event)=> {
+        if(event.target.value === "Yes"){
+            this.hardWorking = true;
+        }else{
+            this.hardWorking = false;
+        }
+    }
+
     decideWorthy = (event) => {
         let clarity = false;
-        let hardWorking = false;
 
         // console.log(this.deserveItRef.current.value);
         let specificActions = this.specficActionsRef.current.value;
 
-        if(specificActions != ""){
+        if(specificActions !== ""){
             clarity = true;
         }
+        console.log(this.hardWorking);
         console.log(clarity);
 
         event.preventDefault();
@@ -22,8 +31,10 @@ class Worthy extends React.Component{
         return (
             <form onSubmit={this.decideWorthy}>
                 <h3>Are you willing to put in the work?</h3>
-                <label><input type="radio" name="deserve-it" value="Yes" ref={this.deserveItRef}/> Yes</label>
-                <label><input type="radio" name="deserve-it" value="No" ref={this.deserveItRef}/> No</label>
+                <div onChange={this.handleWorthy}>
+                    <label><input type="radio" name="deserve-it" value="Yes"/> Yes</label>
+                    <label><input type="radio" name="deserve-it" value="No"/> No</label>
+                </div>
                 <h3>What specific actions are you taking?</h3>
                 <label><input type="text" name="specific-actions" ref={this.specficActionsRef}/></label>
                 <input type="submit"/>
