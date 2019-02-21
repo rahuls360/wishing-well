55cc78dcbde4b0557d894fa10ee9f404cce9cab6
Rahul Makhija <rahuls360@gmail.com>
2019-02-21 14:39:15 +0530

    Updates

---
+++ precache-manifest.36d48805724f439d0ca4a8a0fa28956e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09ec63c43a51ed169412",
+    "url": "/wishing-well/static/js/runtime~main.09ec63c4.js"
+  },
+  {
+    "revision": "a8badcdd42338dc80c7d",
+    "url": "/wishing-well/static/js/main.a8badcdd.chunk.js"
+  },
+  {
+    "revision": "890d7666bc3d8efb4cec",
+    "url": "/wishing-well/static/js/2.890d7666.chunk.js"
+  },
+  {
+    "revision": "a8badcdd42338dc80c7d",
+    "url": "/wishing-well/static/css/main.9d9ca4eb.chunk.css"
+  },
+  {
+    "revision": "2db1cd9e959102b9ac70a572208fda6c",
+    "url": "/wishing-well/index.html"
+  }
+];
